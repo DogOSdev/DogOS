8577cd452ea03224ea40b1c2964c72688e426ab9
Taromaru <doggo@taromaruyuki.xyz>
2021-10-26 22:51:18 -0400

    ✨ .ini file support! (Read only)

--- DogOS/ini.cs
+++ DogOS/ini.cs
@@ -1,23 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Sys = Cosmos.System;
 using System.IO;
+using Sys = Cosmos.System;
 
-namespace CosmosKernel1
+
+namespace DogOS
 {
-    class ini
+    public class IniFile
     {
-        public string file;
-        public ini(string file)
+        // Source code to reading file is here
+        // https://stackoverflow.com/a/55752753
+        public static string Read(string file, string section, string key, string default_value=null)
         {
-            this.file = file;
-        }
+            string[] reader_lines = File.ReadAllLines(file);
+            string current_section = "";
 
-        public string Read(string section, string key)
-        {
-            // TODO: Read file and return value
-            return "";
+            foreach (string reader_line in reader_lines)
+            {
+                if(reader_line.StartsWith('[') && reader_line.EndsWith(']'))
+                {
+                    current_section = reader_line;
+                }
+                else if(current_section.Equals($"[{section}]"))
+                {
+                    string[] line_parts = reader_line.Split(new[] { '=' }, 2);
+
+                    if(line_parts.Length >= 1 && line_parts[0] == key)
+                    {
+                        return line_parts.Length >= 2 ? line_parts[1] : default_value;
+                    }
+                }
+            }
+
+            return default_value;
         }
     }
 }
