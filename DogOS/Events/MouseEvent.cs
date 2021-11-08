c2e9f6e88a5d99caa16f6164f4aa8e01ce20fd0f
Taromaru <doggo@taromaruyuki.xyz>
2021-11-07 23:41:09 -0500

    ✨ Mouse Event

---
+++ DogOS/Events/MouseEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Sys = Cosmos.System;
+
+namespace DogOS.Events
+{
+    public abstract class MouseEvent
+    {
+        public abstract void OnMouseState(Sys.MouseState state);
+        public abstract void OnMouseMove(int x, int y);
+    }
+}
