6ee8612705b100d6676ceb0d4e35c50b6d554ba4
Taromaru <doggo@taromaruyuki.xyz>
2021-11-09 08:39:11 -0500

    ✨ Added GUI Widgets

---
+++ DogOS/GUI/Window.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+using Sys = Cosmos.System;
+
+namespace DogOS.GUI
+{
+    public class Window : CompositeWidget
+    {
+        protected bool dragging = false;
+
+        public Window(Widget parent, int x, int y, int w, int h, Color color) : base(parent, x, y, w, h, color) {}
+
+        public new void OnMouseDown(int x, int y, Sys.MouseState button)
+        {
+            dragging = button == Sys.MouseState.Left;
+            base.OnMouseDown(x, y, button);
+        }
+
+        public new void OnMouseUp(int x, int y, Sys.MouseState button)
+        {
+            dragging = false;
+            base.OnMouseUp(x, y, button);
+        }
+
+        public new void OnMouseMove(int old_x, int old_y, int x, int y)
+        {
+            if(dragging)
+            {
+                this.x += x - old_x;
+                this.y += y - old_y;
+            }
+            base.OnMouseMove(old_x, old_y, x, y);
+        }
+    }
+}
