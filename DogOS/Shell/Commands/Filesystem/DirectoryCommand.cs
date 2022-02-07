c8e04802f7bb92b1b84d0be2487da6613661a324
TaromaruYuki <taromaruyuki@gmail.com>
2022-02-07 12:16:40 -0500

    Update 'dir' help.

--- DogOS/Shell/Commands/Filesystem/DirectoryCommand.cs
+++ DogOS/Shell/Commands/Filesystem/DirectoryCommand.cs
@@ -69,6 +69,7 @@ namespace DogOS.Shell.Commands.Filesystem
             Console.WriteLine(Description);
 
             Console.WriteLine($"\tdir || {Description}");
+            Console.WriteLine($"\tdir [directories]* || Go down the directory tree specified.");
         }
     }
 }
