37aa0d556e92c6158b2a5efbe40f983d4a5011a7
Taromaru <taromaruyuki@gmail.com>
2022-02-14 15:27:47 +0000

    rmfile command (not tested)

---
+++ DogOS/Shell/Commands/Filesystem/RemoveFileCommand.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace DogOS.Shell.Commands.Filesystem
+{
+    class RemoveFileCommand : Command
+    {
+        public RemoveFileCommand() : base("rmfile", "Remove a file") { }
+
+        public override void Execute()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ERR: No file was specified.");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        public override void Execute(List<string> args)
+        {
+            if(File.Exists($"{Kernel.drive}{Kernel.dir}{args[0]}"))
+            {
+                File.Delete($"{Kernel.drive}{Kernel.dir}{args[0]}");
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"ERR: File '{Kernel.drive}{Kernel.dir}{args[0]}' does not exist.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public override void Help()
+        {
+            Console.WriteLine(Description);
+
+            Console.WriteLine($"rmfile [file] || {Description}");
+        }
+    }
+}
